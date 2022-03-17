12ceb17a423f7d86f66039b33c05017c2a957094
ngf93 <nabiullinaguzel93@gmail.com>
2022-03-17 19:17:37 +0300

    adding pages 2

--- src/pages/AddCargo.js
+++ src/pages/AddCargo.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import CustomSelect from '../components/utilities/CustomSelect';
+
+import { IoAddCircle } from 'react-icons/io5';
+import { IconContext } from "react-icons";
 
 export default function AddCargo() {
     return (
@@ -9,31 +13,159 @@ export default function AddCargo() {
                     <div className="col-8">
                         <h4 className="mb-3">Загрузка</h4>
                         <fieldset className="box py-4 px-5">
-                            <div className="row">
+                            <div className="row mb-4">
                                 <div className="col-3">
                                     <div className="title-font fs-12 fw-5">Дата*</div>
                                 </div>
-                                <div className="col-9 d-flex">
-                                    <div className="box w-50 p-3">
-                                        <label className="mb-3">
-                                            <input type="radio" name="frequency" value="Единожды"/>
-                                            <span className="title-font fs-12 fw-5 ms-3">Единожды</span>
-                                        </label>
-                                        <div className="d-flex align-items-center">
-                                            <input type="date" />
-                                            <span className="mx-3">+</span>
-                                            <input type="date" />
+                                <div className="col-9">
+                                    <div className="row">
+                                        <div className="col-7">
+                                            <div className="box p-3">
+                                                <label className="mb-3">
+                                                    <input type="radio" name="frequency" value="Единожды"/>
+                                                    <span className="title-font fs-12 fw-5 ms-3">Единожды</span>
+                                                </label>
+                                                <div className="d-flex align-items-center">
+                                                    <input type="date" />
+                                                    <span className="mx-3">+</span>
+                                                    <CustomSelect className="inp w-100" name="days" checkedOpt="0 дн." options={['0 дн.', '1 дн.']}/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-5">
+                                            <div className="box p-3">
+                                                <label className="mb-3">
+                                                    <input type="radio" name="frequency" value="Постоянно"/>
+                                                    <span className="title-font fs-12 fw-5 ms-3">Постоянно</span>
+                                                </label>
+                                                <CustomSelect className="inp w-100" name="periodicity" checkedOpt="По рабочим дням" options={['По рабочим дням', 'По выходным']}/>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="box w-50 p-3">
-                                        <label className="mb-3">
-                                            <input type="radio" name="frequency" value="Постоянно"/>
-                                            <span className="title-font fs-12 fw-5 ms-3">Постоянно</span>
-                                        </label>
-                                        <div className="d-flex align-items-center">
-                                            <input type="date" />
-                                            <span className="mx-3">+</span>
-                                            <input type="date" />
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Время загрузки</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="d-flex align-items-center">
+                                        <input type="time" />
+                                        <span className="mx-3">—</span>
+                                        <input type="time" />
+                                    </div>
+                                    <label className="mt-2">
+                                        <input type="checkbox" name="frequency" value="Круглосуточно"/>
+                                        <span className="ms-2">Круглосуточно</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Место загрузки*</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="row">
+                                        <div className="col-5">
+                                            <input type="text" placeholder="Населеный пункт"/>
+                                        </div>
+                                        <div className="col-7">
+                                            <input type="text" placeholder="Адрес"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <button type="button" className="green fs-11 fw-5 mt-3 mx-auto d-flex align-items-center">
+                            <IconContext.Provider value={{className: "green icon-15"}}>
+                                <IoAddCircle />
+                            </IconContext.Provider>
+                            <span className="ms-2">Добавить точку загрузки</span>
+                        </button>
+
+                        <h4 className="mt-5 mb-3">Разгрузка</h4>
+                        <fieldset className="box py-4 px-5">
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Дата</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="d-flex align-items-center">
+                                        <input type="date"/>
+                                        <span className="fs-12 mx-3">—</span>
+                                        <input type="date" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Время загрузки</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="d-flex align-items-center">
+                                        <input type="time" />
+                                        <span className="mx-3">—</span>
+                                        <input type="time" />
+                                    </div>
+                                    <label className="mt-2">
+                                        <input type="checkbox" name="frequency" value="Круглосуточно"/>
+                                        <span className="ms-2">Круглосуточно</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Место загрузки*</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="row">
+                                        <div className="col-5">
+                                            <input type="text" placeholder="Населеный пункт"/>
+                                        </div>
+                                        <div className="col-7">
+                                            <input type="text" placeholder="Адрес"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <button type="button" className="green fs-11 fw-5 mt-3 mx-auto d-flex align-items-center">
+                            <IconContext.Provider value={{className: "green icon-15"}}>
+                                <IoAddCircle />
+                            </IconContext.Provider>
+                            <span className="ms-2">Добавить точку разгрузки</span>
+                        </button>
+
+                        <h4 className="mt-5 mb-3">Груз</h4>
+                        <fieldset className="box py-4 px-5">
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Тип груза</div>
+                                </div>
+                                <div className="col-9">
+                                    <CustomSelect className="inp w-100" name="cargo-type"  options={['тип 1', 'тип 2']}/>
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Вес*</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <input type="number" className="weight w-100"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col-3">
+                                    <div className="title-font fs-12 fw-5">Объем*</div>
+                                </div>
+                                <div className="col-9">
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <input type="number" className="size w-100"/>
                                         </div>
                                     </div>
                                 </div>
