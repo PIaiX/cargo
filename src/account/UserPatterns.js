2654217f3e29f51b454d357f5a911f800365fff2
IzmaIs <ichestor0@gmail.com>
2022-07-15 13:51:57 +0300

    Feature: add pagination in profile/patterns

--- src/account/UserPatterns.js
+++ src/account/UserPatterns.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import Pattern from '../components/Pattern';
+import Pagination from "../components/Pagination";
+import usePagination from "../hooks/pagination";
 
 const cars = [
     {
@@ -42,10 +44,90 @@ const cars = [
         aboute: 'Камаз, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
         payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
         contacts: '+ 7 (962) 458 65 79 Тоня',
-    }
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
+    {
+        type: 'car',
+        title: 'Казань-Челны',
+        note: 'Трансмистер',
+        route: 'Казань +50км — Челны +50км',
+        date: 'Ежедневно',
+        aboute: 'Трансмистер, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (999) 999 99 79 Эльвира',
+    },
 ]
 
-const gruzs = [
+const cargos = [
     {
         type: 'cargo',
         title: 'Пермь-Челны-Москва',
@@ -95,12 +177,107 @@ const gruzs = [
         aboute: 'Догма, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
         payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
         contacts: '+ 7 (962) 458 65 79 Арбуз',
-    }
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
+    {
+        type: 'cargo',
+        title: 'Пермь-Челны-Москва',
+        note: 'Трактор',
+        route: 'Казань +50км — Москва +50км',
+        date: 'Ежедневно',
+        aboute: 'Трактор, Грузовик, 20т, 15м3, 13,6/2,45/2,45',
+        payment: 'Возможен торг. С НДС 80 ₽/км, без НДС 76 ₽/км, Предоплата 50%',
+        contacts: '+ 7 (123) 542 22 11 Эльвира',
+    },
 ]
+
+const initialPageLimit = 4
 
 export default function UserPatterns() {
 
     const [tab, setTab] = useState('cars');
+
+    const carsPagination = usePagination(initialPageLimit)
+    const cargoPagination = usePagination(initialPageLimit)
+    const [filterCars, setFilterCars] = useState([])
+    const [filterCargo, setFilterCargo] = useState([])
+    
+    useEffect(() => {
+        const startIndex = (carsPagination.currentPage - 1) * carsPagination.pageLimit
+        const endIndex = startIndex + carsPagination.pageLimit;
+        const paginated = cars.slice(startIndex, endIndex)
+        
+        setFilterCars(paginated)
+        window.scrollTo(0,0)
+    }, [carsPagination.currentPage, carsPagination.pageLimit])
+
+    useEffect(() => {
+        const startIndex = (cargoPagination.currentPage - 1) * cargoPagination.pageLimit
+        const endIndex = startIndex + cargoPagination.pageLimit;
+        const paginated = cargos.slice(startIndex, endIndex)
+
+        setFilterCargo(paginated)
+        window.scrollTo(0,0)
+    }, [cargoPagination.currentPage, cargoPagination.pageLimit])
 
     return (
         <div className='box px-0 p-lg-4 p-xl-5'>
@@ -120,12 +297,12 @@ export default function UserPatterns() {
                     className={(tab === 'cargo') ? 'active tab-btn ms-3 ms-sm-4 ms-xl-5' : 'tab-btn ms-3 ms-sm-4 ms-xl-5'}
                     onClick={() => setTab('cargo')}
                 >
-                    Грузы ({gruzs.length})
+                    Грузы ({cargos.length})
                 </button>
             </div>
             {
                 (tab === 'cars') ?
-                    cars.map((car, index) => (
+                    filterCars.map((car, index) => (
                         <div key={index}>
                             <Pattern
                                 className='mb-3 mb-sm-4'
@@ -141,22 +318,45 @@ export default function UserPatterns() {
                         </div>
                     ))
                     :
-                    gruzs.map((gruz, index) => (
+                    filterCargo.map((cargo, index) => (
                         <div key={index}>
                             <Pattern
                                 className='mb-3 mb-sm-4'
-                                type={gruz.type}
-                                title={gruz.title}
-                                note={gruz.note}
-                                route={gruz.route}
-                                date={gruz.date}
-                                aboute={gruz.aboute}
-                                payment={gruz.payment}
-                                contacts={gruz.contacts}
+                                type={cargo.type}
+                                title={cargo.title}
+                                note={cargo.note}
+                                route={cargo.route}
+                                date={cargo.date}
+                                aboute={cargo.aboute}
+                                payment={cargo.payment}
+                                contacts={cargo.contacts}
                             />
                         </div>
                     ))
             }
+            {tab === 'cars'
+                ?
+                <Pagination
+                    pageLimit={carsPagination.pageLimit}
+                    currentPage={carsPagination.currentPage}
+                    setCurrentPage={carsPagination.setCurrentPage}
+                    pagesDisplayedLimit={3}
+                    itemsAmount={cars.length}
+                    startingPage={carsPagination.startingPage}
+                    setStartingPage={carsPagination.setStartingPage}
+                />
+                :
+                <Pagination
+                    pageLimit={cargoPagination.pageLimit}
+                    currentPage={cargoPagination.currentPage}
+                    setCurrentPage={cargoPagination.setCurrentPage}
+                    pagesDisplayedLimit={3}
+                    itemsAmount={cargos.length}
+                    startingPage={cargoPagination.startingPage}
+                    setStartingPage={cargoPagination.setStartingPage}
+                />
+            }
+
         </div>
     )
 }
