3b4ecd9dc55f973ef15f6b46778a626e708235d6
IzmaIs <ichestor0@gmail.com>
2022-08-25 15:32:11 +0300

    Fix: fixed data in add route/edit route

--- src/pages/EditRoute.js
+++ src/pages/EditRoute.js
@@ -255,6 +255,8 @@ const EditRoute = () => {
 
     const [selectPeriodType, setSelectPeriodType] = useState(null)
 
+    console.log(selectPeriodType)
+
     const loadOptions2 = async (searchKey) => {
 
         const defaultValue = data?.datePeriodType
