6871b4cffc7bd297f9abb39530bbd3a12e5f1b36
And1997Ruz <and1997ruz@gmail.com>
2022-09-01 18:11:07 +0300

    Fix. Fixed global alert positioning

--- src/hooks/alert.js
+++ src/hooks/alert.js
@@ -1,29 +1,36 @@
-import React, {useEffect, useState} from 'react';
-import Alert from 'react-bootstrap/Alert';
-import {useSelector} from 'react-redux';
-import {useDispatch} from 'react-redux/es/exports';
-import {resetAlert} from '../store/actions/alert';
+import React, { useEffect, useState } from "react";
+import Alert from "react-bootstrap/Alert";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux/es/exports";
+import { resetAlert } from "../store/actions/alert";
+import Backdrop from "@mui/material/Backdrop";
 
 const useAlert = (delay) => {
-    const [alertNode, setAlertNode] = useState(null)
-    const submitAlert = useSelector(state => state?.alert?.data)
-    const dispatch = useDispatch()
+  const [alertNode, setAlertNode] = useState(null);
+  const submitAlert = useSelector((state) => state?.alert?.data);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        setAlertNode(
-            <Alert className='submit-alert' variant={submitAlert?.variant} show={submitAlert?.isShow}>
-                {submitAlert?.message}
-            </Alert>
-        )
+  useEffect(() => {
+    setAlertNode(
+        <Backdrop sx={{ color: "#fff", zIndex: 342344 }} open={submitAlert?.isShow} onClick={() => dispatch(resetAlert())}>
+        <Alert
+          className="submit-alert"
+          variant={submitAlert?.variant}
+          show={submitAlert?.isShow}
+        >
+          {submitAlert?.message}
+        </Alert>
+        </Backdrop>
+    );
 
-        if(submitAlert?.isShow) {
-            const timeoutId = setTimeout(() => dispatch(resetAlert()), delay)
+    if(submitAlert?.isShow) {
+        const timeoutId = setTimeout(() => dispatch(resetAlert()), delay)
 
-            return () => clearTimeout(timeoutId)
-        }
-    }, [submitAlert])
+        return () => clearTimeout(timeoutId)
+    }
+  }, [submitAlert]);
 
-    return {alertNode}
-}
+  return { alertNode };
+};
 
-export default useAlert
+export default useAlert;
